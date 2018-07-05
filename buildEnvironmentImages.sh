a88ec87c94dd2204fca9f735c475af0c1bd5ea76
Julian Gründner <juliangruendner@googlemail.com>
2018-07-05 15:33:23 +0200

    change enivonrmentBuild sh

--- buildEnvironmentImages.sh
+++ buildEnvironmentImages.sh
@@ -3,11 +3,11 @@ cd ketos_environment_api
 # build tf image
 cd docker/ubuntu_jupyter_tf
 docker build -t ketos.ai:5043/mlservicecontainer_tf .
-cd ..
+cd ../..
 docker build -f Dockerfile.API.prodTf -t ketos.ai:5043/mlservicecontainer_prod_tf .
 
 # build r image
-cd docker/ubuntu_jupyter_r
+cd docker/alpine_jupyter_r
 docker build -t ketos.ai:5043/mlservicecontainer_r .
-cd ..
+cd ../..
 docker build -f Dockerfile.API.prod -t ketos.ai:5043/mlservicecontainer_prod_r .
