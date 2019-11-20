b171e004a5d04fc91d473f40376845fbb798fecd
Julian Gründner <juliangruendner@googlemail.com>
2019-11-20 23:43:28 +0100

    fix: fix init fhir script - send bundle to base - not resource

--- build_from_images/ketos/init_ketos_fhir.sh
+++ build_from_images/ketos/init_ketos_fhir.sh
@@ -1,3 +1,3 @@
 ./wait-for-it.sh localhost:8080 --timeout=200
-curl -X POST -H "Content-Type: application/json" -d @./ketos_gs_patients.json http://localhost:8080/fhir/Patient
-curl -X POST -H "Content-Type: application/json" -d @./ketos_gs_observations.json http://localhost:8080/fhir/Observation
+curl -X POST -H "Content-Type: application/json" -d @./ketos_gs_patients.json http://localhost:8080/fhir
+curl -X POST -H "Content-Type: application/json" -d @./ketos_gs_observations.json http://localhost:8080/fhir
